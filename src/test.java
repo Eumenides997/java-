179ab68476d8df3c3df87cd481efb1d53ff221a8
Eumenides997 <646224348@qq.com>
2019-05-01 02:33:21 +0800

    第六次上传

--- src/test.java
+++ src/test.java
@@ -3,8 +3,8 @@ import java.util.List;
 
 public class test {
 
-	public static void ConsumerApple(Apple[] apps,Consumer<Apple>c) {
-		for(Apple app:apps) {
+	public static void ConsumerApple(List<Apple> a,Consumer<Apple>c) {
+		for(Apple app:a) {
 			c.accept(app);
 		}
 }
@@ -16,9 +16,18 @@ public class test {
 		ap.setcolor("绿色");
 		ap.setweight(2);
 		a.add(ap);
-		System.out.println(ap.getcolor()+"\n"+ap.getweight());
-		System.out.println(a.get(0).color+"\n"+a.get(0).weight+"\n"+a.get(0).a());
+/*		System.out.println(ap.getcolor()+"\n"+ap.getweight());
+		System.out.println(a.get(0).color+"\n"+a.get(0).weight+"\n"+a.get(0).a());*/
 		
+		ConsumerApple(a,new Consumer<Apple>() {
+
+			@Override
+			public void accept(Apple t) {
+				// TODO Auto-generated method stub
+				System.out.println(t.a());
+			}
+			
+		});//内部类
 		
 }
 }
